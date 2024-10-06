f63367dff066cd50eab10249d856868b26356a60
jordaniza <j@jordaniza.com>
2024-10-06 13:05:12 +0400

    ssh service enabled

--- configuration.nix
+++ configuration.nix
@@ -240,7 +240,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
