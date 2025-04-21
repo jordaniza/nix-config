6faae096030097b25a9839a94f787fa6929f96d5
jordaniza <j@jordaniza.com>
2025-04-21 12:42:16 +0400

    initial devshell

---
+++ dev-shell/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Dev Shell for NixOS";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: {
+    devShell.x86_64-linux = let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+    in
+      pkgs.mkShell {
+        name = "dev-shell";
+
+        buildInputs = [
+          pkgs.git
+          pkgs.bat
+          pkgs.fd
+          pkgs.ripgrep
+          pkgs.fzf
+          pkgs.zsh
+          pkgs.nixvim
+        ];
+
+        shellHook = ''
+          export IN_DEV_SHELL=1
+          source ~/.zshrc
+        '';
+      };
+  };
+}
