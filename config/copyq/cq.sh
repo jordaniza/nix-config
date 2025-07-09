654abafdd96720c46705c97491256b83f3a019f6
jordaniza <j@jordaniza.com>
2025-07-09 12:53:46 +0100

    rm and ls commands

--- config/copyq/cq.sh
+++ config/copyq/cq.sh
@@ -9,22 +9,22 @@ case "$1" in
 	echo "A simple CLI to manage CopyQ."
 	echo ""
 	echo "Commands:"
-	echo "  list        Show a searchable history menu with fzf."
-	echo "  clear       Clear all items from the clipboard history."
-	echo "  [numbers]   Print the content of one or more items by index."
+	echo "  list | ls     Show a searchable history menu with fzf."
+	echo "  clear         Clear all items from the clipboard history."
+	echo "  rm [numbers]  Remove specific items from the clipboard history."
+	echo "  [numbers]     Print the content of one or more items by index."
 	;;
 
-list)
-
+list | ls)
 	# Use a "here document" (<< 'EOF') to pass the script safely to CopyQ
 	selection=$(
-		copyq eval - <<'EOF' | fzf --tac | awk -F. '{print $1}'
+		copyq eval - <<'EOF' | fzf | awk -F. '{print $1}'
 if (size() > 0) {
-var l = [];
-for (var i = 0; i < size(); ++i) {
-l.push(i + ". " + str(read(i)).split("\n")[0]);
-}
-print(l.join("\n"));
+    var l = [];
+    for (var i = 0; i < size(); ++i) {
+	l.push(i + ". " + str(read(i)).split("\n")[0]);
+    }
+    print(l.join("\n"));
 }
 EOF
 	)
@@ -40,6 +40,16 @@ clear)
 		copyq remove $(seq 0 $((count - 1)))
 	fi
 	echo "CopyQ history cleared."
+	;;
+rm)
+	shift # Discards the 'rm' argument
+	if [ "$#" -gt 0 ]; then
+		copyq remove "$@"
+		echo "Removed item(s): $@"
+	else
+		echo "Usage: cq rm [index1] [index2] ..."
+		echo "Error: No item indices provided."
+	fi
 	;;
 
 *)
