66a204a6d22d7ff90c2daccdb3279f10c52ba9e2
jordaniza <j@jordaniza.com>
2025-09-21 09:44:22 +0100

    update to 24.11

--- config/nixvim/plugins/conform.nix
+++ config/nixvim/plugins/conform.nix
@@ -1,25 +1,27 @@
 {
   enable = true;
-  formattersByFt = {
-    css = ["prettierd" "prettier"];
-    html = ["prettierd" "prettier"];
-    javascript = ["prettierd" "prettier"];
-    javascriptreact = ["prettier"];
-    json = ["prettier"];
-    lua = ["stylua"];
-    markdown = ["prettier"];
-    nix = ["alejandra"];
-    python = ["black"];
-    rust = ["rustfmt"];
-    # bit janky atm - put a prettierrc in the root and it works
-    solidity = ["prettier"];
-    sh = ["shfmt"];
-    typescript = ["prettierd" "prettier"];
-    typescriptreact = ["prettier"];
-    yaml = ["prettierd" "prettier"];
-  };
-  formatOnSave = {
-    lspFallback = true;
-    timeoutMs = 2000;
+  settings = {
+    formatters_by_ft = {
+      css = ["prettierd" "prettier"];
+      html = ["prettierd" "prettier"];
+      javascript = ["prettierd" "prettier"];
+      javascriptreact = ["prettier"];
+      json = ["prettier"];
+      lua = ["stylua"];
+      markdown = ["prettier"];
+      nix = ["alejandra"];
+      python = ["black"];
+      rust = ["rustfmt"];
+      # bit janky atm - put a prettierrc in the root and it works
+      solidity = ["prettier"];
+      sh = ["shfmt"];
+      typescript = ["prettierd" "prettier"];
+      typescriptreact = ["prettier"];
+      yaml = ["prettierd" "prettier"];
+    };
+    format_on_save = {
+      lsp_fallback = true;
+      timeout_ms = 2000;
+    };
   };
 }
