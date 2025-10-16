50ae11b9d564d4e9aa5f741252ded43ff53cb425
jordaniza <j@jordaniza.com>
2025-10-16 13:39:03 +0400

    add config

--- configuration.nix
+++ configuration.nix
@@ -143,7 +143,7 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   security.sudo = {
     enable = true;
